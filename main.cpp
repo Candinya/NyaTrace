bf6242b6b5f67d9f9d52b9cb235f2d1daa2242a7
Nya Candy <dev@candinya.com>
2022-12-08 19:45:34 +0800

    chore: output log to file in release mode

--- main.cpp
+++ main.cpp
@@ -3,9 +3,46 @@
 #include <QApplication>
 #include <QDir>
 #include <QFile>
+#include <QDateTime>
+
+void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+   Q_UNUSED(context);
+
+   QString dt = QDateTime::currentDateTime().toString("dd/MM/yyyy hh:mm:ss");
+   QString txt = QString("[%1] ").arg(dt);
+
+   switch (type)
+   {
+      case QtDebugMsg:
+         txt += QString("{Debug} \t\t %1").arg(msg);
+         break;
+      case QtWarningMsg:
+         txt += QString("{Warning} \t %1").arg(msg);
+         break;
+      case QtCriticalMsg:
+         txt += QString("{Critical} \t %1").arg(msg);
+         break;
+      case QtFatalMsg:
+         txt += QString("{Fatal} \t\t %1").arg(msg);
+         abort();
+         break;
+   }
+
+   QFile outFile("LogFile.log");
+   outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+
+   QTextStream textStream(&outFile);
+   textStream << txt << endl;
+}
 
 int main(int argc, char *argv[])
 {
+#ifndef QT_DEBUG
+    // 控制日志输出
+    qInstallMessageHandler(customMessageHandler);
+#endif
+
     // 定义版本号
     auto version = QString("NyaTrace %1").arg(APP_VERSION);
 
